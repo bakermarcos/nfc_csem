1f7267e1ecc52093a22a74f94a6a704869066781
bakermarcos <marcos.baker@hotmail.com>
2021-04-07 12:25:19 -0300

    another change

--- lib/main.dart
+++ lib/main.dart
@@ -71,8 +71,7 @@ class _NFCHomeState extends State<NFCHome> with TickerProviderStateMixin {
     Wakelock.enable();
     setState(() {
       _stream = NFC
-          .readNDEF(once: true,
-                            throwOnUserCancel: false,)
+          .readNDEF()
           .listen((NDEFMessage message) {
         if (message.isEmpty) {
           print("Read empty NDEF message");
